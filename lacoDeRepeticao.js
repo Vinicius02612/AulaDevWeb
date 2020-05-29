fd6b57245719f362abec7fac5d7eff6e4f216cb2
Vinicius02612 <57628027+Vinicius02612@users.noreply.github.com>
2020-05-29 17:14:05 -0300

    Update lacoDeRepeticao.js

--- lacoDeRepeticao.js
+++ lacoDeRepeticao.js
@@ -5,9 +5,3 @@ for (let i = 0; i < 30; i++) {
 }
 // percorrendo um  Arrey...
 
-const aluno = ["vinicius, clecio, franciel, lara, fatima"]
-for (let j = 0; j < aluno.length; j++) {
-    //pegando o nome do ultimo elemento do arrey..
-    aluno.push("fatima")
-    console.log(aluno)
-}
