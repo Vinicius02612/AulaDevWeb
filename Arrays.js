e8334fc416b67d17c245710730dfb403c2e7d63a
Vinicius02612 <57628027+Vinicius02612@users.noreply.github.com>
2020-05-29 23:52:48 -0300

    Add files via upload

---
+++ Arrays.js
@@ -0,0 +1,17 @@
+//usando arrays em javaScript...
+
+var alunos = ["vinicius", "Franciel", "fatima", "Lara"]
+    //mostrando tamanho do Array...
+console.log(alunos.length)
+    //removeu o aluno lara
+alunos.pop()
+console.log(alunos)
+    //adicionar um aluno na ultima posçao
+alunos.push("Lara")
+console.log(alunos)
+    // removendo do inicio
+alunos.shift()
+console.log(alunos)
+    //adicionar ao inicio do array...
+alunos.unshift("Salvador")
+console.log(alunos)
